79c549cce9d45259ea2cb8bd278115c42a4d15f9
AbdAlmjed Skaik <94318021+abdalmjeds2000@users.noreply.github.com>
2024-08-13 23:23:26 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,17 @@ app.use(cors({
       methods: ['GET', 'POST'],
       allowedHeaders: ['Content-Type']
 }));
-
+app.use((req, res, next) => {
+  //allow access from every, elminate CORS
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.removeHeader('x-powered-by');
+  //set the allowed HTTP methods to be requested
+  res.setHeader('Access-Control-Allow-Methods','POST');
+  //headers clients can use in their requests
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  //allow request to continue and be handled by routes
+  next();
+});
 
 const username = process.env.USERNAME,
       password = process.env.PASSWORD,

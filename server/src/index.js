dbff1fd0bffc14abb5a0385bcae7f20fce7c01f3
AbdAlmjed Skaik <94318021+abdalmjeds2000@users.noreply.github.com>
2024-08-13 23:31:52 +0300

    .

--- server/src/index.js
+++ server/src/index.js
@@ -4,34 +4,27 @@ const cors = require("cors");
 const router = require("./router");
 
 const app = express();
-const corsOptions = {
+
+// Use CORS middleware
+app.use(cors({
   origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
-};
-app.use(cors(corsOptions));
+}));
+
 app.use(express.json());
 app.use(router);
-app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.removeHeader('x-powered-by');
-  //set the allowed HTTP methods to be requested
-  res.setHeader('Access-Control-Allow-Methods','POST');
-  //headers clients can use in their requests
-  res.setHeader('Access-Control-Allow-Headers','Content-Type');
-  //allow request to continue and be handled by routes
-  next();
-});
 
-const username = process.env.USERNAME,
-      password = process.env.PASSWORD,
-      db = process.env.DB,
-      port = process.env.PORT;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const db = process.env.DB;
+const port = process.env.PORT;
 
 mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.uoup6yr.mongodb.net/${db}?retryWrites=true&w=majority`)
   .then(() => {
     console.log("Server Run!");
     app.listen(port);
+  })
+  .catch(err => {
+    console.error("Failed to connect to MongoDB", err);
   });
-

0b830004451fbac7edd87b7afc11a853f4936caf
AbdAlmjed Skaik <94318021+abdalmjeds2000@users.noreply.github.com>
2024-08-13 23:48:13 +0300

    .

--- server/src/router.js
+++ server/src/router.js
@@ -9,9 +9,8 @@ const updateTodoRoute = require('./routes/updateTodoRoute');
 const deleteTodoRoute = require('./routes/deleteTodoRoute');
 
 const isLoggedIn = require('./middleware/isLoggedIn');
-router.post("/", (req, res) => {
-  return res.json({ status: 200, message: "AbdAlmjed ToDo API!" });
-});
+
+router.post("/", (req, res) => "AbdAlmjed ToDo API!");
 router.post("/register", registerRoute);
 router.post("/login", loginRoute);
 router.get("/todos", isLoggedIn, readTodosRoute);

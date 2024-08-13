78e9e69626eaeca9b5bbc9bdfcc63c8a06e49be3
AbdAlmjed Skaik <94318021+abdalmjeds2000@users.noreply.github.com>
2024-08-13 23:20:17 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 app.use(cors({
-      origin: "https://abdalmjed-todo.vercel.app/",
+      origin: "https://abdalmjed-todo.vercel.app",
       methods: ["POST", "GET"],
       credentials: true
 }));
